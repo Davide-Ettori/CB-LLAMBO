8d809d364cd2f410cea7e3c2f9760e3bf5f83949
Davide-Ettori <ettodavi01@gmail.com>
2026-01-13 20:52:03 +0100

    quick

---
+++ run_bayesmark_breast_all_models_ucb1_tuned.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+trap "kill -- -$BASHPID" EXIT
+
+# Run all Bayesmark models on breast dataset using UCB1-Tuned surrogate mode.
+if [ "${OPENAI_API_TYPE}" = "azure" ]; then
+    ENGINE="gpt35turbo_20230727"
+else
+    ENGINE="gpt-3.5-turbo"
+fi
+
+DATASET="breast"
+
+for model in "RandomForest" "SVM" "DecisionTree" "MLP_SGD" "AdaBoost"
+do
+    python3 exp_bayesmark/run_bayesmark.py --dataset $DATASET --model $model --num_seeds 1 --sm_mode discriminative --sm_eval_mode bandit_ucb1_tuned --engine $ENGINE
+    sleep 60
+done
